6c7f18910e91bf1ae71feda2c96ba882f7a759da
Nayem-Hasan <nayeemhasanpr@gmail.com>
2024-07-09 23:51:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import { connectToDatabase } from "./connectToDatabase.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT;
+
+// middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to root Server');
+});
+
+
+// router middleware
+app.use("/api/auth", authRoutes);
+
+function errorHandler(err, req, res, next) {
+    console.log('error = ', err);
+    if (res.headersSent) {
+        next('error - headers already sent');
+    } else if (err?.message) {
+        res.status(500).send({ message: err.message });
+    } else {
+        res.status(500).send({
+            message: 'there was an error, express did not give any message for this error',
+        });
+    }
+}
+
+app.use(errorHandler);
+
+app.listen(port, async () => {
+    await connectToDatabase();
+    console.log(`response address is = http://localhost:${port}`);
+});
---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { login, register } from "../controllers/authController.js";
+import { checkLogin } from "../middlewares/checkLogin.js";
+
+const Route = express.Router();
+Route.post('/register', register);
+Route.post('/login', login);
+Route.get('/test', checkLogin, (req, res) => {
+    res.send({ data: 'This is private route. This means, you have successfully logged in' });
+});
+
+export default Route;
