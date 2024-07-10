d03515af2c716100be517b0d0d2a9e71fecb8bb2
Nayem-Hasan <nayeemhasanpr@gmail.com>
2024-07-11 02:52:05 +0600

    working

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { checkLogin } from '../middlewares/common/checkLogin';
-import { create } from '../controllers/locationController';
+import { checkLogin } from '../middlewares/common/checkLogin.js';
+import { create } from '../controllers/locationController.js';
+import { upload } from '../utils/multer.js';
 
 const Route = express.Router();
 Route.post('/', checkLogin, upload.single('logo'), create);
