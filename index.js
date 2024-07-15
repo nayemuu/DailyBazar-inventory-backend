0e4d0f131b0888c84f365813cb517ac3e560b319
Nayem-Hasan <nayeemhasanpr@gmail.com>
2024-07-15 20:47:21 +0600

    working

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import morgan from "morgan";
-import fs from "fs";
-import path from "path";
-import { connectToDatabase } from "./connectToDatabase.js";
-import authRoutes from "./routes/authRoutes.js";
-import locationRoutes from "./routes/locationRoutes.js";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+import { connectToDatabase } from './connectToDatabase.js';
+import authRoutes from './routes/authRoutes.js';
+import locationRoutes from './routes/locationRoutes.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 // middlewares
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to root Server");
+app.get('/', (req, res) => {
+  res.send('Welcome to root Server = ', __dirname);
 });
 
 // router middleware
-app.use("/api/auth", authRoutes);
-app.use("/api/location", locationRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/location', locationRoutes);
 
 // error handling middleware
 function errorHandler(err, req, res, next) {
-  console.log("error = ", err);
+  console.log('error = ', err);
   if (res.headersSent) {
-    next("error - headers already sent");
+    next('error - headers already sent');
   } else if (err?.message) {
     res.status(500).send({ message: err.message });
   } else {
     res.status(500).send({
       message:
-        "there was an error, express did not give any message for this error",
+        'there was an error, express did not give any message for this error',
     });
   }
 }
